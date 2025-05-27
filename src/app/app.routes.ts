b0c2b28249d0f99c1ec2ebbaee91a4e8a04f9fe3
Guiziin227 <gh63388@gmail.com>
2025-05-26 21:19:17 -0300

    add navigation toolbar and routing for cadastro and consulta components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'cadastro', loadComponent: () => import('./cadastro/cadastro.component').then(m => m.CadastroComponent)},
+  {path: 'consulta', loadComponent: () => import('./consulta/consulta.component').then(m => m.ConsultaComponent)},
+];
