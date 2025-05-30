8c7cb115cb1821eef18698b415d1a3b944b25479
Guiziin227 <gh63388@gmail.com>
2025-05-30 16:59:23 -0300

    feat: add snackbar notifications for client registration and update in cadastro component

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, inject} from '@angular/core';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatCardModule} from '@angular/material/card';
 import {FormsModule} from '@angular/forms';
@@ -6,6 +6,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBar} from '@angular/material/snack-bar';
 import {Cliente} from './cliente';
 import {ClienteService} from '../cliente.service';
 import {ActivatedRoute, Router} from '@angular/router';
@@ -32,6 +33,7 @@ export class CadastroComponent implements OnInit {
 
   cliente: Cliente = Cliente.newCliente();
   actualization: boolean = false;
+  snackBar: MatSnackBar = inject(MatSnackBar);
 
   constructor(
     private clienteService: ClienteService,
@@ -58,9 +60,16 @@ export class CadastroComponent implements OnInit {
     if (!this.actualization) {
       this.clienteService.singUpCliente(this.cliente);
       this.cliente = Cliente.newCliente();
+      this.openSnackBar("Cliente cadastrado com sucesso!");
+      this.router.navigate(['/consulta']);
     } else {
       this.clienteService.updateCliente(this.cliente);
       this.router.navigate(['/consulta']);
+      this.openSnackBar("Cliente atualizado com sucesso!");
     }
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, "Ok");
   }
 }
