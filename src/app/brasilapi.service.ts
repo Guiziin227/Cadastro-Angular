1c53d747c16679a25018f558ec1e01745c907acc
Guiziin227 <gh63388@gmail.com>
2025-05-30 17:33:08 -0300

    feat: implement HTTP client in BrasilapiService for fetching UFs

--- src/app/brasilapi.service.ts
+++ src/app/brasilapi.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Estado} from './brasil.models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BrasilapiService {
 
-  constructor() { }
+  baseUrl: string = 'https://brasilapi.com.br/api';
+
+  constructor(private http: HttpClient) { }
+
+  listUfs(): Observable<Estado[]> {
+    return this.http.get<Estado[]>(`${this.baseUrl}/ibge/uf/v1`);
+  }
 }
