c811a91d77bb78c85db2ef4664ad109d6acd7bdd
Guiziin227 <gh63388@gmail.com>
2025-05-30 15:31:48 -0300

    feat: add client update functionality and navigation in cadastro component

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -8,7 +8,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {Cliente} from './cliente';
 import {ClienteService} from '../cliente.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 
 @Component({
@@ -33,6 +33,7 @@ export class CadastroComponent implements OnInit {
   constructor(
     private clienteService: ClienteService,
     private route: ActivatedRoute,
+    private router: Router,
   ) {
   }
 
@@ -51,7 +52,12 @@ export class CadastroComponent implements OnInit {
   }
 
   singUpCliente() {
-    this.clienteService.singUpCliente(this.cliente);
-    this.cliente = Cliente.newCliente();
+    if (!this.actualization) {
+      this.clienteService.singUpCliente(this.cliente);
+      this.cliente = Cliente.newCliente();
+    } else{
+      this.clienteService.updateCliente(this.cliente);
+      this.router.navigate(['/consulta']);
+    }
   }
 }
